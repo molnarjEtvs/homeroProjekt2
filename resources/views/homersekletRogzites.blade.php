a0e354f5be38e05c55ae6544ff141200f1bb87ea
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2022-10-18 13:59:59 +0200

    Hőmérséklet form

--- resources/views/homersekletRogzites.blade.php
+++ resources/views/homersekletRogzites.blade.php
@@ -2,5 +2,31 @@
 @section('title','Főoldal')
 @section('content')
      @include('menu')
-    <h1>Hőmérséklet rögzítés</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="bg-primary p-3 text-white">
+                    <h1>Hőmérséklet rögzítése</h1>
+                    <form method="POST">
+                        @csrf
+                        <div class="my-3">
+                            <label for="t_id">Terem száma:</label>
+                            <select name="t_id" id="t_id" class="form-select">
+                                <option value="0">Válassz!</option>
+                            </select>
+                        </div>
+
+                        <div class="my-3">
+                            <label for="homerseklet">Hőmérséklet (celsisus):</label>
+                            <input type="number" class="form-control" name="homerseklet" id="homerseklet">
+                        </div>
+
+                        <div class="my-3">
+                            <button type="submit" class="btn btn-light">Rögzítés</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
