e3bb7192c2a21fdc9dddb9c599c5f93daff22ef6
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2022-10-18 13:13:07 +0200

    Validáció kész

--- app/Http/Controllers/TeremRogzites.php
+++ app/Http/Controllers/TeremRogzites.php
@@ -14,13 +14,30 @@ class TeremRogzites extends Controller
 
         $validate = $req->validate(
             [
-                "nev" => "required|min:3"
+                "nev" => "required|min:3",
+                "szel_cm" => "required|numeric|min:100|max:5000",
+                "hossz_cm" => "required|numeric|min:100|max:5000",
+                "mag_cm" => "required|numeric|min:100|max:500"
             ],
             [
                 "nev.required" => "A mező kitöltése kötelező!",
-                "nev.min" => "Minimum 3 karakter adj meg!"
+                "nev.min" => "Minimum 3 karakter adj meg!",
+                "szel_cm.required" => "A mező kitöltése kötelező!",
+                "szel_cm.numeric" => "Csak szám értéket lehet beírni",
+                "szel_cm.min" => "Minimum 100",
+                "szel_cm.max" => "Maximim 5000",
+                "hossz_cm.required" => "A mező kitöltése kötelező!",
+                "hossz_cm.numeric" => "Csak szám értéket lehet beírni",
+                "hossz_cm.min" => "Minimum 100",
+                "hossz_cm.max" => "Maximim 5000",
+                "mag_cm.required" => "A mező kitöltése kötelező!",
+                "mag_cm.numeric" => "Csak szám értéket lehet beírni",
+                "mag_cm.min" => "Minimum 100",
+                "mag_cm.max" => "Maximim 500"
             ]
         );
+
+        
 
     }
 }
--- resources/views/teremRogzites.blade.php
+++ resources/views/teremRogzites.blade.php
@@ -19,17 +19,26 @@
 
                         <div class="mt-3 mb-3">
                             <label for="szel_cm">Terem szélessége:</label>
-                            <input type="number" name="szel_cm" id="szel_cm" class="form-control">    
+                            <input type="number" value="{{ old('szel_cm') }}" name="szel_cm" id="szel_cm" class="form-control">    
+                            @error('szel_cm')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
 
                         <div class="mt-3 mb-3">
                             <label for="hossz_cm">Terem hosszúság:</label>
-                            <input type="number" name="hossz_cm" id="hossz_cm" class="form-control">    
+                            <input type="number" value="{{ old('hossz_cm') }}" name="hossz_cm" id="hossz_cm" class="form-control"> 
+                            @error('hossz_cm')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror   
                         </div>
 
                         <div class="mt-3 mb-3">
                             <label for="mag_cm">Terem magasság:</label>
-                            <input type="number" name="mag_cm" id="mag_cm" class="form-control">    
+                            <input type="number" value="{{ old('mag_cm') }}" name="mag_cm" id="mag_cm" class="form-control">  
+                            @error('mag_cm')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror  
                         </div>
                         
                         <div class="mt-3 mb-3">
